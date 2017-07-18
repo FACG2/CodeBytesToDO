3be730d9ebc19ca9fc6e6e44b820f6b6347a8ae8
Your Name <youremail@domain.com>
2017-07-18 14:07:34 +0300

    first two test Relates #3

--- skeleton/test.js
+++ skeleton/test.js
@@ -1,7 +1,23 @@
 var test = require('tape');
-var logic = require('./logic');
+var todoFunctions= require('./logic');
 
 test('Example test', function(t) {
-  t.pass();
+  var actual =  todoFunctions.addTodo([],{description:'make'});
+  var expected = [{description:"make",id:1,state:false}];
+  t.deepEqual(actual, expected, 'Should return new array');
   t.end();
+});
+
+test('Example test', function(t) {
+  var old=[{description:"make",id:1,state:true},{description:"make",id:2,state:false}];
+  var newone=[{description:"make",id:1,state:true},{description:"make",id:2,state:true}];
+  console.log(old);
+  var actual =  todoFunctions.markTodo(old,2);
+  var expected = newone;
+  console.log(newone);
+  console.log(old);
+  t.deepEqual(actual, expected, 'True');
+  t.end();
+
+  // console.log(newone);
 });
